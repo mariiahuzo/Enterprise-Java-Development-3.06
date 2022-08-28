3eddade559f7ea03642f64617134d64024b73659
mariiahuzo <d.mariya.d@gmail.com>
2022-08-28 11:50:52 +0200

    initial commit

---
+++ src/main/java/com/IRONHACK/SpringFile/SpringFileApplication.java
@@ -0,0 +1,13 @@
+package com.IRONHACK.SpringFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileApplication.class, args);
+	}
+
+}
